785f226067f59e2584e7a253f386928e02928796
bharatheshagouda-cs <bharatheshagoudacs@gmail.com>
2025-10-21 22:57:46 +0530

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -7,7 +7,7 @@ Script Purpose:
     The Gold layer represents the final dimension and fact tables (Star Schema)
 
     Each view performs transformations and combines data from the Silver layer 
-    to produce a clean, enriched, and business-ready dataset.
+    to produce a clean, enriched, and business-ready dataset. hmfhngf      
 
 Usage:
     - These views can be queried directly for analytics and reporting.
